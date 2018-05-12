d8730b68eb0b6a0a6a90e4347087c9b602f9b729
Alexander <agonz519@gmail.com>
2018-05-12 01:16:29 -0400

    reverted some changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 // Set up Express
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
